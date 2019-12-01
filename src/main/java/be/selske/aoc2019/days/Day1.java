e6784fcb2bf26898b49fb71317993d92427f7f9a
Kevin Sels <kevin.sels@vrt.be>
2019-12-01 10:18:27 +0100

    day1

---
+++ src/main/java/be/selske/aoc2019/days/Day1.java
@@ -0,0 +1,44 @@
+package be.selske.aoc2019.days;
+
+import be.selske.aoc2019.AocDay;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.function.ToIntFunction;
+
+public class Day1 extends AocDay {
+
+    public static void main(String[] args) {
+        new Day1().solve(Day1.class.getResourceAsStream("/inputs/day1"));
+    }
+
+    private Day1() {
+        super(
+                (input) -> solve(Day1::fuelNeeded, input),
+                (input) -> solve(Day1::rocketEquation, input)
+        );
+    }
+
+    private static String solve(ToIntFunction<Integer> fuelCalculator, InputStream input) {
+        return new BufferedReader(new InputStreamReader(input))
+                .lines()
+                .map(Integer::parseInt)
+                .mapToInt(fuelCalculator)
+                .sum() + "";
+    }
+
+    private static int fuelNeeded(int payload) {
+        return (payload / 3) - 2;
+    }
+
+    private static int rocketEquation(int payload) {
+        int fuelNeeded = fuelNeeded(payload);
+        if (fuelNeeded <= 0) {
+            return 0;
+        } else {
+            return fuelNeeded + rocketEquation(fuelNeeded);
+        }
+    }
+
+}
