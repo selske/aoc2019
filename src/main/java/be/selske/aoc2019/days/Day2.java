33748c2ca1c8f7cf08369ad4d1dcfd1d6fff7139
Kevin Sels <kevin.sels@vrt.be>
2019-12-02 09:14:40 +0100

    cleaned up imports

--- src/main/java/be/selske/aoc2019/days/Day2.java
+++ src/main/java/be/selske/aoc2019/days/Day2.java
@@ -7,10 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
