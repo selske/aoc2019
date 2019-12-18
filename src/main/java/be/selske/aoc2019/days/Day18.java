ceeb557c806a7135e21c3f40eb61656b4348d891
Kevin Sels <kevin.sels@vrt.be>
2019-12-18 08:14:45 +0100

    day 18 parse and edge finding

---
+++ src/main/java/be/selske/aoc2019/days/Day18.java
@@ -0,0 +1,118 @@
+package be.selske.aoc2019.days;
+
+import be.selske.aoc2019.AocDay;
+import be.selske.aoc2019.Coordinate;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.toList;
+
+public class Day18 extends AocDay {
+
+    public static void main(String[] args) {
+        new Day18().solve(() -> Day18.class.getResource("/inputs/day18.txt"));
+    }
+
+    private Day18() {
+        super(
+                Day18::part1,
+                Day18::part2
+        );
+    }
+
+    private static String part1(Stream<String> input) {
+        Map<Coordinate, Character> map = parseInput(input);
+
+        Set<Coordinate> visited = new HashSet<>();
+        int distanceTravelled = 0;
+        List<Coordinate> edges = List.of(map.entrySet().stream().filter(e -> e.getValue() == '@').findAny().map(Map.Entry::getKey).orElseThrow());
+        while (!edges.isEmpty()) {
+            List<Coordinate> newEdges = edges.stream()
+                    .flatMap(coordinate -> Arrays.stream(Movement.values()).map(m -> m.newCoordinateGetter.apply(coordinate)))
+                    .filter(not(visited::contains))
+                    .filter(coordinate -> map.getOrDefault(coordinate, '#') != '#')
+                    .collect(toList());
+            visited.addAll(edges);
+            edges = newEdges;
+            distanceTravelled++;
+            if (newEdges.stream().anyMatch(edge -> map.getOrDefault(edge, '.') != '.')) {
+                System.out.println("edge after = " + distanceTravelled);
+            }
+        }
+
+//        print(map, new HashSet<>(), distanceTravelled);
+        return null;
+    }
+
+    private static String part2(Stream<String> input) {
+        return null;
+    }
+
+    private static Map<Coordinate, Character> parseInput(Stream<String> input) {
+        Map<Coordinate, Character> map = new HashMap<>();
+        List<String> rows = input.collect(Collectors.toList());
+        for (int r = 0; r < rows.size(); r++) {
+            String row = rows.get(r);
+            for (int c = 0; c < row.toCharArray().length; c++) {
+                if ('#' == row.toCharArray()[c]) {
+                    continue;
+                }
+                map.put(new Coordinate(c, -1 * r), row.toCharArray()[c]);
+            }
+        }
+        return map;
+    }
+
+    @SuppressWarnings("unused")
+    private enum Movement {
+        NORTH(1, Coordinate::getUp),
+        SOUTH(2, Coordinate::getDown),
+        WEST(3, Coordinate::getLeft),
+        EAST(4, Coordinate::getRight);
+
+        private final long inputValue;
+        private final Function<Coordinate, Coordinate> newCoordinateGetter;
+
+        Movement(long inputValue, Function<Coordinate, Coordinate> newCoordinateGetter) {
+            this.inputValue = inputValue;
+            this.newCoordinateGetter = newCoordinateGetter;
+        }
+    }
+
+    private static void print(Map<Coordinate, Character> map, Set<Coordinate> visited, int distanceTravelled) {
+        int left = Integer.MAX_VALUE;
+        int top = Integer.MIN_VALUE;
+        int right = Integer.MIN_VALUE;
+        int bottom = Integer.MAX_VALUE;
+
+        for (Coordinate coordinate : map.keySet()) {
+            if (coordinate.getX() < left) left = coordinate.getX();
+            if (coordinate.getX() > right) right = coordinate.getX();
+            if (coordinate.getY() > top) top = coordinate.getY();
+            if (coordinate.getY() < bottom) bottom = coordinate.getY();
+        }
+        left--;
+        top++;
+        right++;
+        bottom--;
+
+        for (int row = top; row >= bottom; row--) {
+            for (int col = left; col <= right; col++) {
+                Character value = map.getOrDefault(new Coordinate(col, row), '#');
+                if (visited.contains(new Coordinate(col, row))) {
+                    value = ' ';
+                }
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+        System.out.println("distanceTravelled = " + distanceTravelled);
+        System.out.println();
+    }
+
+}
+
